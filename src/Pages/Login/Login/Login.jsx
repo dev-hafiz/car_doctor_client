620e34055e39a1c5bed877c4571808d6a64613dc
okshi <johnstarhafiz@gmail.com>
2023-08-16 23:57:21 +0600

    Implement functionality to get user information using form

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -2,56 +2,71 @@ import loginImg from "../../../assets/images/login/login.svg";
 import facebook from "../../../assets/icons/Facebook.png";
 import google from "../../../assets/icons/google.png";
 import { Link } from "react-router-dom";
+
 const Login = () => {
+  const handleLoginData = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    // const email = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email);
+    console.log(password);
+  };
+
   return (
     <div className="mt-12 mb-36 grid grid-cols-1 mid:grid-cols-1 lg:grid-cols-2">
       <div className="flex ml-14 items-center">
         <img src={loginImg} alt="loginImage" />
       </div>
       <div>
-        <div className="card h-screen max-w-xl	 flex-shrink-0  shadow-2xl bg-white">
+        <div className="card h-full max-w-xl	 flex-shrink-0  shadow-2xl bg-white">
           <div className="card-body mt-8 ">
             <h1 className="text-4xl text-center text-[#444444]">Login</h1>
-            <div className="form-control mt-6">
-              <label className="label">
-                <span className="label-text text-dark font-semibold">
-                  Email
-                </span>
-              </label>
-              <input
-                type="email"
-                placeholder="Your email"
-                name="email"
-                required
-                className="input bg-white input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text text-dark font-semibold">
-                  Confirm Password
-                </span>
-              </label>
-              <input
-                type="password"
-                placeholder="Your password"
-                name="password"
-                required
-                className="input bg-white input-bordered"
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <input
-                className="btn capitalize text-xl border-none outline-none text-white bg-[#FF3811]"
-                type="submit"
-                value="Login"
-              />
-            </div>
+
+            <form onSubmit={handleLoginData}>
+              <div className="form-control mt-6">
+                <label className="label">
+                  <span className="label-text text-dark font-semibold">
+                    Email
+                  </span>
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Your email"
+                  className="input bg-white input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-4">
+                <label className="label">
+                  <span className="label-text text-dark font-semibold">
+                    Confirm Password
+                  </span>
+                </label>
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Your password"
+                  className="input bg-white input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn capitalize text-xl border-none outline-none text-white bg-[#FF3811]"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
 
             <div>
               <h3 className="text-center my-4">or Sign In with</h3>
@@ -62,7 +77,7 @@ const Login = () => {
                 <button>
                   <img src={google} alt="facebbok login" />
                 </button>
-                <p className="mt-14">
+                <p className="mt-8">
                   Have an accounts?{" "}
                   <Link className="text-[#FF3811] font-bold" to="/signin">
                     Sign In
