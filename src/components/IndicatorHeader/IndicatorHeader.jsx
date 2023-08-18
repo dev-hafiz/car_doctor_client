9f40267dd4a86748039b36fa4741b2882f4a3ea0
okshi <johnstarhafiz@gmail.com>
2023-08-18 12:27:06 +0600

    Used DaisyUI compnent for indicator header

--- src/components/IndicatorHeader/IndicatorHeader.jsx
+++ src/components/IndicatorHeader/IndicatorHeader.jsx
@@ -1,7 +1,17 @@
 const IndicatorHeader = () => {
   return (
-    <div>
-      <h1>From Indicator</h1>
+    <div className="hero h-screen bg-white">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 };
