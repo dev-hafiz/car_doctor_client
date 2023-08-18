883c3321d1b5ab6d9fa9d7e3005e7b026a7517da
okshi <johnstarhafiz@gmail.com>
2023-08-18 12:24:07 +0600

    Create an indicator header component

--- src/Pages/ServiceDetails/ServiceDeatils.jsx
+++ src/Pages/ServiceDetails/ServiceDeatils.jsx
@@ -1,7 +1,9 @@
+import IndicatorHeader from "../../components/IndicatorHeader/IndicatorHeader";
+
 const ServiceDeatils = () => {
   return (
     <div className="h-screen">
-      <h1>This is Service Details</h1>
+      <IndicatorHeader />
     </div>
   );
 };
---
+++ src/components/IndicatorHeader/IndicatorHeader.jsx
@@ -0,0 +1,9 @@
+const IndicatorHeader = () => {
+  return (
+    <div>
+      <h1>From Indicator</h1>
+    </div>
+  );
+};
+
+export default IndicatorHeader;
