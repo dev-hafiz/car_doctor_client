66d1f8e23ff4cb52f2aca2d0a4c0a471216e3fff
okshi <johnstarhafiz@gmail.com>
2023-08-28 23:48:20 +0600

    Complete engineer team

--- src/Pages/Home/OurTeam/OurTeam.jsx
+++ src/Pages/Home/OurTeam/OurTeam.jsx
@@ -1,3 +1,11 @@
+import c1 from "../../../assets/images/teamCarousel/c1.png";
+import c2 from "../../../assets/images/teamCarousel/c2.png";
+import c3 from "../../../assets/images/teamCarousel/c3.png";
+import f from "../../../assets/images/socialIcon/f.png";
+import t from "../../../assets/images/socialIcon/t.png";
+import g from "../../../assets/images/socialIcon/g.png";
+import i from "../../../assets/images/socialIcon/in (2).png";
+
 const OurTeam = () => {
   return (
     <div className="mb-24">
@@ -13,20 +21,83 @@ const OurTeam = () => {
 
       <div className="carousel w-full">
         <div id="slideFirst" className="carousel-item relative w-full">
-          <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="flex flex-cols-1">
+            <div className="card w-96 bg-white shadow-xl">
               <figure className="px-10 pt-10">
-                <img
-                  src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                  alt="Shoes"
-                  className="rounded-xl"
-                />
+                <img src={c1} alt="Shoes" className="rounded-xl" />
               </figure>
               <div className="card-body items-center text-center">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions">
-                  <button className="btn btn-primary">Buy Now</button>
+                <h2 className="text-[#444444] font-bold text-2xl mt-[-20px]">
+                  Thomas Henry
+                </h2>
+                <p className="text-[#737373] font-semibold">
+                  Branded Tyre Expert
+                </p>
+                <div className="space-x-3 ">
+                  <button>
+                    <img src={f} alt="" />
+                  </button>
+                  <button>
+                    <img src={t} alt="" />
+                  </button>
+                  <button>
+                    <img src={g} alt="" />
+                  </button>
+                  <button>
+                    <img src={i} alt="" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-96 bg-white shadow-xl">
+              <figure className="px-10 pt-10">
+                <img src={c2} alt="Shoes" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="text-[#444444] font-bold text-2xl mt-[-20px]">
+                  Marshal John
+                </h2>
+                <p className="text-[#737373] font-semibold">
+                  Dent Paint Expert
+                </p>
+                <div className="space-x-3 ">
+                  <button>
+                    <img src={f} alt="" />
+                  </button>
+                  <button>
+                    <img src={t} alt="" />
+                  </button>
+                  <button>
+                    <img src={g} alt="" />
+                  </button>
+                  <button>
+                    <img src={i} alt="" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-96 bg-white shadow-xl">
+              <figure className="px-10 pt-10">
+                <img src={c3} alt="Shoes" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="text-[#444444] font-bold text-2xl mt-[-20px]">
+                  Max Alfred
+                </h2>
+                <p className="text-[#737373] font-semibold">Engine Expert</p>
+                <div className="space-x-3 ">
+                  <button>
+                    <img src={f} alt="" />
+                  </button>
+                  <button>
+                    <img src={t} alt="" />
+                  </button>
+                  <button>
+                    <img src={g} alt="" />
+                  </button>
+                  <button>
+                    <img src={i} alt="" />
+                  </button>
                 </div>
               </div>
             </div>
@@ -41,10 +112,88 @@ const OurTeam = () => {
           </div>
         </div>
         <div id="slideNext" className="carousel-item relative w-full">
-          <img
-            src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-            className="w-full"
-          />
+          <div className="flex flex-cols-1">
+            <div className="card w-96 bg-white shadow-xl">
+              <figure className="px-10 pt-10">
+                <img src={c1} alt="Shoes" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="text-[#444444] font-bold text-2xl mt-[-20px]">
+                  Thomas Henry
+                </h2>
+                <p className="text-[#737373] font-semibold">
+                  Branded Tyre Expert
+                </p>
+                <div className="space-x-3 ">
+                  <button>
+                    <img src={f} alt="" />
+                  </button>
+                  <button>
+                    <img src={t} alt="" />
+                  </button>
+                  <button>
+                    <img src={g} alt="" />
+                  </button>
+                  <button>
+                    <img src={i} alt="" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-96 bg-white shadow-xl">
+              <figure className="px-10 pt-10">
+                <img src={c3} alt="Shoes" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="text-[#444444] font-bold text-2xl mt-[-20px]">
+                  Max Alfred
+                </h2>
+                <p className="text-[#737373] font-semibold">Engine Expert</p>
+                <div className="space-x-3 ">
+                  <button>
+                    <img src={f} alt="" />
+                  </button>
+                  <button>
+                    <img src={t} alt="" />
+                  </button>
+                  <button>
+                    <img src={g} alt="" />
+                  </button>
+                  <button>
+                    <img src={i} alt="" />
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="card w-96 bg-white shadow-xl">
+              <figure className="px-10 pt-10">
+                <img src={c2} alt="Shoes" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="text-[#444444] font-bold text-2xl mt-[-20px]">
+                  Marshal John
+                </h2>
+                <p className="text-[#737373] font-semibold">
+                  Dent Paint Expert
+                </p>
+                <div className="space-x-3 ">
+                  <button>
+                    <img src={f} alt="" />
+                  </button>
+                  <button>
+                    <img src={t} alt="" />
+                  </button>
+                  <button>
+                    <img src={g} alt="" />
+                  </button>
+                  <button>
+                    <img src={i} alt="" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slideFirst" className="btn btn-circle">
               ❮
