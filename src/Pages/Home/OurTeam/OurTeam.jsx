45509b523273673e46cb037c354c22635312bb32
okshi <johnstarhafiz@gmail.com>
2023-08-28 20:42:41 +0600

    Used team card in slider

--- src/Pages/Home/OurTeam/OurTeam.jsx
+++ src/Pages/Home/OurTeam/OurTeam.jsx
@@ -12,30 +12,44 @@ const OurTeam = () => {
       </div>
 
       <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-            className="w-full"
-          />
+        <div id="slideFirst" className="carousel-item relative w-full">
+          <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure className="px-10 pt-10">
+                <img
+                  src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                  alt="Shoes"
+                  className="rounded-xl"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
+            <a href="#slideNext" className="btn btn-circle">
               ❮
             </a>
-            <a href="#slide2" className="btn btn-circle">
+            <a href="#slideNext" className="btn btn-circle">
               ❯
             </a>
           </div>
         </div>
-        <div id="slide2" className="carousel-item relative w-full">
+        <div id="slideNext" className="carousel-item relative w-full">
           <img
             src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
             className="w-full"
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">
+            <a href="#slideFirst" className="btn btn-circle">
               ❮
             </a>
-            <a href="#slide1" className="btn btn-circle">
+            <a href="#slideNext" className="btn btn-circle">
               ❯
             </a>
           </div>
