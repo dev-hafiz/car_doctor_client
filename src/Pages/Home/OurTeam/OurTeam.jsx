ee0a1b50e7715ef855008bc06ad42c2616250773
okshi <johnstarhafiz@gmail.com>
2023-08-29 22:29:30 +0600

    Fixed second slider width and height issue

--- src/Pages/Home/OurTeam/OurTeam.jsx
+++ src/Pages/Home/OurTeam/OurTeam.jsx
@@ -112,7 +112,7 @@ const OurTeam = () => {
           </div>
         </div>
         <div id="slideNext" className="carousel-item relative w-full">
-          <div className="flex flex-cols-1">
+          <div className="flex flex-cols-1 items-center space-x-16 ml-20">
             <div className="card w-72 bg-white shadow-xl">
               <figure className="px-10 pt-10">
                 <img src={c1} alt="Shoes" className="rounded-xl" />
