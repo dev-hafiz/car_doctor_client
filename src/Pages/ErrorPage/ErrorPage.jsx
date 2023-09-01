242b87af971f7451ae26698fa7f6bbaa1f84976c
okshi <johnstarhafiz@gmail.com>
2023-09-01 22:57:04 +0600

    Center error page emage

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -2,7 +2,7 @@ import errorImg from "../../assets/images/error.png";
 
 const ErrorPage = () => {
   return (
-    <div className="h-screen text-center">
+    <div className="h-screen flex justify-center items-center">
       <img src={errorImg} alt="errorPage" />
     </div>
   );
